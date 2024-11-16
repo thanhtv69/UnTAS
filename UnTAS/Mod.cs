1e350c616d9a5e5761b3e482b8a414d685423a69
thanhtv69 <thanh55dhv@gmail.com>
2024-11-16 17:37:27 +0700

    adddd

--- UnTAS/Mod.cs
+++ UnTAS/Mod.cs
@@ -9,23 +9,14 @@ namespace UnTAS
 {
     public class Mod : MelonMod
     {
-        public static float prevTimeScale = 1f;
-        public static float baseTimeScale = 1.3f;
-        public static bool frozen = false;
+        public static float baseTimeScale = 1f + 1f / 3f;
 
-        public static KeyCode SlowKey = KeyCode.F1;
-        public static KeyCode FastKey = KeyCode.F2;
-        public static KeyCode PauseKey = KeyCode.R;
-        public static KeyCode ResetKey = KeyCode.M;
+        public static KeyCode SlowKey = KeyCode.N; // Giảm tốc
+        public static KeyCode FastKey = KeyCode.M; // Tăng tốc
+        public static KeyCode ResetKey = KeyCode.R; // Reset tốc độ
 
-        public static void DrawFrozenText()
-        {
-            GUI.Label(new Rect((Screen.width / 2) - 150, (Screen.height / 2) - 150, 1000, 200), "<b><color=white><size=100>Frozen</size></color></b>");
-        }
-        
         public static void DrawWatermark()
         {
-            //GUI.Label(new Rect((Screen.width / 2) - 120, Screen.height - 60, 1000, 200), "<b><color=red><size=20>Speed:" + baseTimeScale + "</size></color></b>");
             GUI.Label(new Rect(40, Screen.height - 60, 1000, 200), "<b><color=red><size=20>Speed:" + baseTimeScale + "</size></color></b>");
         }
 
@@ -36,61 +27,33 @@ namespace UnTAS
 
         public override void OnUpdate()
         {
-            if(!frozen)
+            if (Input.GetKeyDown(SlowKey))
             {
-                if (Input.GetKeyDown(SlowKey))
+                if (Time.timeScale > 0.1f)
                 {
-                    if (Time.timeScale > 0.1f)
-                    {
-                        baseTimeScale -= 0.1f;
-                        MelonLogger.Msg("Speed: " + baseTimeScale);
-                    }
-                }
-
-                if (Input.GetKeyDown(FastKey))
-                {
-                    baseTimeScale += 0.1f;
+                    baseTimeScale -= 0.1f;
                     MelonLogger.Msg("Speed: " + baseTimeScale);
                 }
             }
 
-            if(Input.GetKeyDown(PauseKey))
+            if (Input.GetKeyDown(FastKey))
             {
-                ToggleFreeze();
+                baseTimeScale += 0.1f;
+                MelonLogger.Msg("Speed: " + baseTimeScale);
             }
 
-            if(Input.GetKeyDown(ResetKey))
+            if (Input.GetKeyDown(ResetKey))
             {
-                baseTimeScale = 1f;
+                baseTimeScale = 1f + 1f / 3f;
                 MelonLogger.Msg("Speed: " + baseTimeScale);
             }
 
             Time.timeScale = baseTimeScale;
         }
 
-        private static void ToggleFreeze()
-        {
-            frozen = !frozen;
-
-            if (frozen)
-            {
-                MelonEvents.OnGUI.Subscribe(DrawFrozenText, 100);
-                prevTimeScale = baseTimeScale;
-                baseTimeScale = 0f;
-            }
-            else
-            {
-                MelonEvents.OnGUI.Unsubscribe(DrawFrozenText);
-                baseTimeScale = prevTimeScale;
-            }
-        }
-
         public override void OnDeinitializeMelon()
         {
-            if (frozen)
-            {
-                ToggleFreeze();
-            }
+            // Không cần xử lý đóng băng, đã xoá logic liên quan
         }
     }
 }
